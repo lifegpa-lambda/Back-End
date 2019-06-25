88f7c42dcfbb6968d1b55ef54b51443e0cfb05a0
zchristy <zchristy44@gmail.com>
2019-06-25 11:19:15 -0500

    Added .returning(*) to habits and users seeds data

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -30,6 +30,6 @@ exports.seed = function(knex, Promise) {
           userImgUrl:
             'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
         },
-      ]);
+      ]).returning('*')
     });
 };
